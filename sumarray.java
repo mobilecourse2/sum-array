7f5b237f80fe5a7ca614d9c0044a7afd3ba0f1cc
Mobile Application Course <44363418+mobilecourse2@users.noreply.github.com>
2018-10-24 12:22:24 +0500

    Add files via upload

---
+++ sumarray.java
@@ -0,0 +1,19 @@
+public class sumarray{
+	
+	public static void main(String[] args){
+		
+		int []arr={1,2,3,4,5};
+		//int []arr2={6,7,8,9,10};
+		int sum =0;
+		for(int i=0;i<arr.length;i++){
+			
+			sum +=arr[i];
+			
+			
+		}
+		System.out.print(sum);
+		
+		
+	}
+	
+}
